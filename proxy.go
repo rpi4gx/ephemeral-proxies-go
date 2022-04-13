ba3a9f725e79f0aff1bf9958d8b388b5a6b2af7a
rpi4gx <rpi4gx@gmail.com>
2022-04-13 17:48:34 +0200

    added ExtendExpirationTime()

--- proxy.go
+++ proxy.go
@@ -11,31 +11,31 @@ import (
 )
 
 type ProxyVisibility struct {
-	Ip         string `json:"ip"`
-	Country    string `json:"country"`
-	CountryISO string `json:"country_iso"`
-	IsCountryInEurope bool `json:"country_eu"`
-	Latitude float32 `json:"latitude"`
-	Longitude float32 `json:"longitude"`
-	Timezone string `json:"timezone"`
-	ASN string `json:"asn"`
-	ASNOrganization string `json:"asn_org"`
-	ZipCode string `json:"zip_code"`
-	RegionName string `json:"region_name"`
-	RegionCode string `json:"region_code"`
-	City string `json:"city"`
+	Ip                string  `json:"ip"`
+	Country           string  `json:"country"`
+	CountryISO        string  `json:"country_iso"`
+	IsCountryInEurope bool    `json:"country_eu"`
+	Latitude          float32 `json:"latitude"`
+	Longitude         float32 `json:"longitude"`
+	Timezone          string  `json:"timezone"`
+	ASN               string  `json:"asn"`
+	ASNOrganization   string  `json:"asn_org"`
+	ZipCode           string  `json:"zip_code"`
+	RegionName        string  `json:"region_name"`
+	RegionCode        string  `json:"region_code"`
+	City              string  `json:"city"`
 }
 
 type ProxyFeaturesSupportedProtocols struct {
 	Socks4 bool `json:"sock4"`
 	Socks5 bool `json:"sock5"`
-	Http bool `json:"http"`
-	Https bool `json:"https"`
+	Http   bool `json:"http"`
+	Https  bool `json:"https"`
 }
 
 type ProxyFeatures struct {
-	IsStatic bool `json:"static"`
-	SupportedProtocols  ProxyFeaturesSupportedProtocols `json:"supported_protocols"`
+	IsStatic           bool                            `json:"static"`
+	SupportedProtocols ProxyFeaturesSupportedProtocols `json:"supported_protocols"`
 }
 
 type Proxy struct {
@@ -43,9 +43,9 @@ type Proxy struct {
 	Host           string          `json:"host"`
 	Port           int             `json:"port"`
 	ExpirationTime time.Time       `json:"expires_at"`
-	WhitelistedIps []string `json:"whitelisted_ips"`
+	WhitelistedIps []string        `json:"whitelisted_ips"`
 	Visibility     ProxyVisibility `json:"visibility"`
-	Features ProxyFeatures `json:"features"`
+	Features       ProxyFeatures   `json:"features"`
 	apiKey         string
 }
 
@@ -58,6 +58,30 @@ type proxyApiResponse struct {
 func (p *Proxy) String() string {
 	r, _ := json.MarshalIndent(p, "", "    ")
 	return string(r)
+}
+
+func processProxyApiResponse(req *http.Request) (*proxyApiResponse, error) {
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	if res.StatusCode != 200 {
+		return nil, errors.New("http response: " + strconv.Itoa(res.StatusCode))
+	}
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+	var p proxyApiResponse
+	if err := json.Unmarshal(body, &p); err != nil {
+		return nil, err
+	}
+	if !p.Success {
+		return nil, errors.New("api failure: " + p.Message)
+	}
+	return &p, nil
 }
 
 func NewProxy(apiKey string) (*Proxy, error) {
@@ -83,32 +107,33 @@ func NewProxyWithOptions(apiKey string, countriesISO []string, extraWhitelistIp 
 		req.URL.RawQuery = query.Encode()
 	}
 
-	res, err := http.DefaultClient.Do(req)
+	p, err := processProxyApiResponse(req)
 	if err != nil {
 		return nil, err
-	}
-	defer res.Body.Close()
-
-	if res.StatusCode != 200 {
-		return nil, errors.New("http response: " + strconv.Itoa(res.StatusCode))
-	}
-	body, err := ioutil.ReadAll(res.Body)
-	if err != nil {
-		return nil, err
-	}
-
-	var p proxyApiResponse
-	if err := json.Unmarshal(body, &p); err != nil {
-		return nil, err
-	}
-	if !p.Success {
-		return nil, errors.New("api failure: " + p.Message)
 	}
 
 	p.Proxy.apiKey = apiKey
 	return &p.Proxy, nil
 }
 
-func (p *Proxy) ExtendExpirationTime() error {
+func (proxy *Proxy) ExtendExpirationTime() error {
+	apiKey := proxy.apiKey
+	url := "https://ephemeral-proxies.p.rapidapi.com/v1/extend_proxy"
+	req, _ := http.NewRequest("GET", url, nil)
+
+	req.Header.Add("X-RapidAPI-Host", "ephemeral-proxies.p.rapidapi.com")
+	req.Header.Add("X-RapidAPI-Key", apiKey)
+
+	query := req.URL.Query()
+	query.Add("id", proxy.Id)
+	req.URL.RawQuery = query.Encode()
+
+	p, err := processProxyApiResponse(req)
+	if err != nil {
+		return err
+	}
+
+	*proxy = p.Proxy
+	proxy.apiKey = apiKey
 	return nil
 }
