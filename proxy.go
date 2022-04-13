c2571bb4ee0c742a00f55a7cd417cdbb14e14763
rpi4gx <rpi4gx@gmail.com>
2022-04-13 09:08:24 +0200

    added proxy with options

--- proxy.go
+++ proxy.go
@@ -6,15 +6,23 @@ import (
 	"io/ioutil"
 	"net/http"
 	"strconv"
+	"strings"
 	"time"
 )
 
-// TODO: complete
+// # TODO: complete
+type ProxyVisibility struct {
+	Ip         string `json:"ip"`
+	Country    string `json:"country"`
+	CountryISO string `json:"country_iso"`
+}
+
 type Proxy struct {
-	Id             string    `json:"id"`
-	Host           string    `json:"host"`
-	Port           int       `json:"port"`
-	ExpirationTime time.Time `json:"expirest_at"`
+	Id             string          `json:"id"`
+	Host           string          `json:"host"`
+	Port           int             `json:"port"`
+	ExpirationTime time.Time       `json:"expirest_at"` // TODO: fix time parsing
+	Visibility     ProxyVisibility `json:"visibility"`
 	apiKey         string
 }
 type proxyApiResponse struct {
@@ -24,15 +32,32 @@ type proxyApiResponse struct {
 }
 
 func (p *Proxy) String() string {
-	return "Proxy: " + p.Id + " Host: " + p.Host + " Port: " + strconv.Itoa(p.Port)
+	r, _ := json.MarshalIndent(p, "", "    ")
+	return string(r)
 }
 
-func GetProxy(apiKey string) (*Proxy, error) {
+func NewProxy(apiKey string) (*Proxy, error) {
+	return NewProxyWithOptions(apiKey, []string{}, "")
+}
+
+func NewProxyWithOptions(apiKey string, countriesISO []string, extraWhitelistIp string) (*Proxy, error) {
 	url := "https://ephemeral-proxies.p.rapidapi.com/v1/proxy"
 	req, _ := http.NewRequest("GET", url, nil)
 
 	req.Header.Add("X-RapidAPI-Host", "ephemeral-proxies.p.rapidapi.com")
 	req.Header.Add("X-RapidAPI-Key", apiKey)
+
+	if len(countriesISO) > 0 {
+		query := req.URL.Query()
+		query.Add("countries", strings.Join(countriesISO, ","))
+		req.URL.RawQuery = query.Encode()
+	}
+
+	if len(extraWhitelistIp) > 0 {
+		query := req.URL.Query()
+		query.Add("whitelist_ip", extraWhitelistIp)
+		req.URL.RawQuery = query.Encode()
+	}
 
 	res, err := http.DefaultClient.Do(req)
 	if err != nil {
