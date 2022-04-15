9d5bb7d9bde7d4062534773c67c26380ff43c3a9
rpi4gx <rpi4gx@gmail.com>
2022-04-15 22:03:18 +0200

    examples update

--- _examples/get_proxy.go
+++ _examples/get_proxy.go
@@ -12,25 +12,17 @@ func usage() {
 	fmt.Println("required arguments")
 	fmt.Println("\t--key=RAPIDAPI_KEY")
 }
+
 func main() {
 
 	var apiKey = flag.String("key", "", "User's RapidAPI Key")
 	flag.Parse()
 
-	// Ensure user has provided their RapidAPI key
+	// Ensures user has provided their RapidAPI key
 	if len(*apiKey) == 0 {
 		usage()
 		os.Exit(-1)
 	}
-
-	// Obtains information about the current state of the Ephemeral Proxies API service
-	serviceStatus, err := ephemeralproxies.GetServiceStatus(*apiKey)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Ephemeral Proxies Service Status:")
-	fmt.Println(serviceStatus)
-	fmt.Println()
 
 	// Obtains a proxy available for the next 30 mins
 	proxy, err := ephemeralproxies.NewProxy(*apiKey)
---
+++ _examples/service_status.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+
+	ephemeralproxies "github.com/rpi4gx/ephemeral-proxies-go"
+)
+
+func usage() {
+	fmt.Println("required arguments")
+	fmt.Println("\t--key=RAPIDAPI_KEY")
+}
+
+func main() {
+
+	var apiKey = flag.String("key", "", "User's RapidAPI Key")
+	flag.Parse()
+
+	// Ensure user has provided their RapidAPI key
+	if len(*apiKey) == 0 {
+		usage()
+		os.Exit(-1)
+	}
+
+	// Obtains information about the current state of the Ephemeral Proxies API service
+	serviceStatus, err := ephemeralproxies.GetServiceStatus(*apiKey)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Ephemeral Proxies Service Status:")
+	fmt.Println(serviceStatus)
+	fmt.Println()
+}
